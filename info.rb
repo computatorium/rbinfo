dcc9c98b9913a2e48bcf28837cc37eade9517667
malcjones <33630760+malcjones@users.noreply.github.com>
2025-12-04 12:45:48 -0600

    added shebang & \t

--- info.rb
+++ info.rb
@@ -1,3 +1,6 @@
+#!/usr/bin/env ruby
+
+# complete as of 3.4.7 (2025-10-08 revision revision 7a5688e2a2)
 Info = {
   :copyright => RUBY_COPYRIGHT,
   :description => RUBY_DESCRIPTION,
@@ -7,12 +10,12 @@ Info = {
   :platform => RUBY_PLATFORM,
   :release_date => RUBY_RELEASE_DATE,
   :revision => RUBY_REVISION,
-  :version => RUBY_VERSION
+  :version => RUBY_VERSION,
 }
 
 if __FILE__ == $0 
   puts "rbinfo:"
   Info.each do |key, value|
-    puts "  #{key}: #{value}"
+    puts "\t#{key}: #{value}"
   end
 end
